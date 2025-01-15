179ef4a93f78d248d502040b7a4adb1325303c6c
pawel.piwowarczyk@allegro.com <pawel.piwowarczyk@allegro.com>
2025-01-15 10:28:28 +0100

    feat: set configuration

--- ci/build.sh
+++ ci/build.sh
@@ -1,1 +1,11 @@
-yarn husky init
+#!/bin/bash
+set -e
+
+export NODE_OPTIONS="--max-old-space-size=4096"
+export COREPACK_ENABLE_DOWNLOAD_PROMPT=0
+
+yarn install --immutable
+yarn tsc 
+yarn prettier:check 
+yarn lint:all 
+yarn test:all 
